31436aa7ed9254a62ea453c141fb26fcd7be48b9
MURAOKA Taro <koron.kaoriya@gmail.com>
2018-02-24 14:21:34 +0900

    improve test

--- test/foo_test.go
+++ test/foo_test.go
@@ -3,13 +3,24 @@ package foo
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/tama-go/assert"
 )
 
 type foo Foo
 
-func TestNew(t *testing.T) {
+func init() {
+	assert.Add(&foo{}, cmpopts.IgnoreFields(foo{}, "Int", "Rnd"))
+}
+
+func TestStruct(t *testing.T) {
 	a := New("aaa", 123)
 	b := New("aaa", 123)
+	assert.FatalEqual(t, a, b)
+}
+
+func TestTypeAlias(t *testing.T) {
+	a := (*foo)(New("aaa", 123))
+	b := (*foo)(New("aaa", 456))
 	assert.FatalEqual(t, a, b)
 }
